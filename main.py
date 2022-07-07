d5e77c13d6a456903d0c2b5532400f1a6cc9096b
Apurba Adhikari <erikduke182@gmail.com>
2022-07-07 10:28:42 +0545

    Basic Code written in main.py

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-from .parser import PDFParser
-from dotenv import dotenv_values
 import os
+import sys
+from parser import PDFParser
+from dotenv import dotenv_values
 
 CONFIG = dotenv_values(".env")
-PATH = os.path.join(os.getcwd(), CONFIG["FOLDER_NAME"], CONFIG["FILE_NAME"])
+
+try:
+    PATH = os.path.join(os.getcwd(), CONFIG["FOLDER_NAME"], CONFIG["FILE_NAME"])
+
+except KeyError:
+    print("Error: The values in .env doesn't exist")
+    print("Please create the .env file or fill 'FILE_NAME' and 'FOLDER_NAME' for location in it")
+    sys.exit(-1)
+
 
 
 def main():
-    
     file = open(PATH, "rb")
+
+    try:
+        file = open(PATH, "rb")
+
+    except FileNotFoundError:
+        print("Error: The file doesn't exist in the given folder")
+        print("Please change the .env and try again")
+        sys.exit(-1)
+
     parser = PDFParser(file)
     parser.parse()
+    parser.printData()
 
 
 
