624b4ea26e914282c420d347d374b8b7f70200a6
Apurba Adhikari <erikduke182@gmail.com>
2022-07-07 10:29:03 +0545

    Project structure changed

--- parser.py
+++ parser.py
@@ -6,6 +6,11 @@ class PDFParser:
     def __init__(self, file: IO):
         self.file = file 
         self.data = []
+        
 
     def parse(self):
+        pass
+
+
+    def printData(self):
         pass
